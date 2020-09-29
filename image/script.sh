3338ff8aa49fbfabc4780e2055baf499f365f21b
Ilya Melnik <Ilya_Melnik1@epam.com>
2020-09-29 19:24:48 +0300

    homework1: packer(add unit-file)

--- image/script.sh
+++ image/script.sh
@@ -3,29 +3,9 @@ sudo mkdir -p /opt/webapp-server/bin /opt/webapp-server/conf/
 sudo curl -L -o /opt/webapp-server/bin/webapp-server https://bit.ly/2GjP1QG
 sudo chmod +x /opt/webapp-server/bin/webapp-server
 
+# Interolate variable
+envsubst < /tmp/webapp-server.conf | sudo tee /opt/webapp-server/conf/webapp-server.conf
 
-# Edit config file
-sudo sed -i "s|^name=.*|name=${FirstName} ${LastName}|" /tmp/file.conf
-sudo sed -i "s|^logo_url=.*|logo_url=${logo_url}|" /tmp/file.conf
-sudo mv /tmp/file.conf /opt/webapp-server/conf/webapp-server.conf
-
-
-# Create unit-file
-cat << EOF | sudo tee /etc/systemd/system/webapp-server.service
-[Unit]
-Description=Simple WebApp Server
-After=network.target
-
-[Service]
-Type=simple
-EnvironmentFile=-/opt/webapp-server/conf/webapp-server.conf
-ExecStart=/opt/webapp-server/bin/webapp-server
-ExecStop=/bin/kill -s QUIT $MAINPID
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-
+sudo mv /tmp/webapp-server.service /etc/systemd/system/webapp-server.service
 # Start webserver 
 sudo systemctl start webapp-server && sudo systemctl enable webapp-server
