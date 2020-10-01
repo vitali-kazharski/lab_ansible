754257de4cb96a5c40241ed3dd03ffea00ed0e3e
Ilya Melnik <Ilya_Melnik1@epam.com>
2020-10-01 11:19:43 +0300

    empty

--- task2/ad-hoc-commands.sh
+++
@@ -1,3 +0,0 @@
-ansible webapp-server -i inventory.orig -b -u centos -m user -a "name=devops home=/home/devops"
-ansible webapp-server -i inventory.orig -b -u centos -m copy -a "src=files/devops.sudoers dest=/etc/sudoers.d/devops"
-ansible webapp-server -i inventory.orig -b -u centos -m authorized_key -a "user=devops key={{ lookup('file', 'files/devops.ssh.id-rsa.pub')}}"
