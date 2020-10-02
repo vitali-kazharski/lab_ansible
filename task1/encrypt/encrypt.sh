cdd9d1e912ebb393cbfb349ddf3a425124b5e13d
Ilya Melnik <Ilya_Melnik1@epam.com>
2020-10-02 19:25:29 +0300

    start

--- task1/encrypt/encrypt.sh
+++
@@ -1,4 +0,0 @@
-ansible-vault encrypt_string --vault-id dev@.passwords 'Password12345' --name 'secret1' > vars.yaml
-ansible-vault encrypt_string --vault-id test@.passwords 'Password3456' --name 'secret2' >> vars.yaml
-ansible-vault encrypt_string --vault-id stage@.passwords 'phul3AiFai0Ebei5' --name 'secret3' >> vars.yaml
-ansible-vault encrypt_string --vault-id prod@.passwords 'kah7ahLu Eed1ohch Sa0hoh1W' --name 'secret4' >> vars.yaml
